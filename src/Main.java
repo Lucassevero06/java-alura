ebc753001fb56c5dd74ae893b9151a7c984bb536
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-23 15:24:57 -0400

    evoluindo em java, entendendo os tipos de dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Esse é o Screen Match");
+
+        String nomeDoFilme = "Top Gun - Maverick";
+        System.out.println("Filme: " + nomeDoFilme);
+
+        int anoDeLancamento= 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        // media calculada pelas tres notas existentes
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme dos anos 90";
+        System.out.println(sinopse);
     }
 }
